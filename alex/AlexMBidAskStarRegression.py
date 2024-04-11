9ae2c25beaf921193da21b2fdfc81a2a3833474a
AlexMorrow239 <alex.morrow239@gmail.com>
2024-04-11 15:58:47 -0400

    Most current version of star strategy

--- alex/AlexMBidAskStarRegression.py
+++ alex/AlexMBidAskStarRegression.py
@@ -1,9 +1,7 @@
-from typing import Dict, List
+from typing import List
 from datamodel import OrderDepth, TradingState, Order
 import collections
 from collections import defaultdict
-import random
-import math
 import copy
 import numpy as np
 import jsonpickle
@@ -14,10 +12,11 @@ import jsonpickle
 class Trader:
     def __init__ (self):
         self.position = copy.deepcopy({'STARFRUIT': 0, 'AMETHYSTS': 0})
-        self.star_cache = []
-        self.star_poly_order = 8
-        self.SMOOTHING = 0.98
+        self.star_window_size = 10
+        self.SMOOTHING = 0.9
         self.cpnl = defaultdict(lambda: 0)
+        self.star_cache_bid = []
+        self.star_cache_ask = []
     
     def filter(self, z: List) -> List:
             z_doubled = z
@@ -36,23 +35,18 @@ class Trader:
         
 
     def calc_next_price(self):
-        # Data from 4 days (possible overfitting)
-        # coef = [3.72536029254, -1.613913639929, -5.209307802657288, 0.00943688103602776]
-        # intercept = 4959.376030787304
 
-        # Data from 1 day
-        # coef = [2.194822134563818, -3.526493888652451, 1.8195854259998546, -0.0002055528195619442]
-        # intercept = 5035.728800687614
-        smoothed_star_cache = self.filter(self.star_cache)
-        # a,b,c,d,intercept = np.polyfit(range(0, len(smoothed_star_cache)), smoothed_star_cache, 4)
-        a,b,c,d,intercept = np.polyfit(range(0, len(smoothed_star_cache)), smoothed_star_cache, 4)
+        smoothed_star_cache_bid = self.filter(self.star_cache_bid)
+        a,b,c,d,intercept = np.polyfit(range(0, len(smoothed_star_cache_bid)), smoothed_star_cache_bid, 4)
+        t = len(self.star_cache_bid) + self.star_window_size/4
+        next_price_bid = (a * t**4) + (b * t**3) + (c * t**2) + (d * t) + intercept
+
+        smoothed_star_cache_ask = self.filter(self.star_cache_ask)
+        a,b,c,d,intercept = np.polyfit(range(0, len(smoothed_star_cache_ask)), smoothed_star_cache_ask, 4)
+        t = len(self.star_cache_ask) + self.star_window_size/4
+        next_price_ask = (a * t**4) + (b * t**3) + (c * t**2) + (d * t) + intercept
         
-        # next_price = (coef[0] * self.star_cache[-1]) + (coef[1] * self.star_cache[-2]) + (coef[2] * self.star_cache[-3]) + (coef[3] * self.star_cache[-4]) + intercept
-        # next_price = (coef[0] * self.star_cache[0]) + (coef[1] * self.star_cache[1]) + (coef[2] * self.star_cache[2]) + (coef[3] * self.star_cache[3]) + intercept
-        t = len(self.star_cache) + self.star_poly_order/4
-        next_price = (a * t**4) + (b * t**3) + (c * t**2) + (d * t) + intercept
-        
-        return int(round(next_price))
+        return int(round(next_price_bid)), int(round(next_price_ask))
     
     def create_orders_regression(self, product, order_depth, acc_bid, acc_ask, LIMIT):
         orders: list[Order] = []
@@ -81,12 +75,6 @@ class Trader:
                 orders.append(Order(product, bid, quantity)) if cum_sell_quant >= -20 else None
         
         self.position['STARFRUIT'] = cur_position
-
-        # undercut_buy = best_buy + 1
-        # undercut_sell = best_sell - 1
-
-        # bid_price = min(undercut_buy, acc_bid)
-        # ask_price = max(undercut_sell, acc_ask)
         
         return orders
 
@@ -98,35 +86,36 @@ class Trader:
         if state.traderData:
             saved_state = jsonpickle.decode(state.traderData)
             self.position = saved_state.position
-            self.star_cache = saved_state.star_cache
-            self.star_poly_order = saved_state.star_poly_order
+            self.star_window_size = saved_state.star_window_size
             self.SMOOTHING = saved_state.SMOOTHING
             self.cpnl = saved_state.cpnl
         
         self.position['STARFRUIT'] = state.position.get('STARFRUIT', 0) # Update position
         print(f"OFFICIAL POSITION: {self.position['STARFRUIT']}")
 
-        # Ensure coefficients do not exceed 12, remove oldest mid_price if it does
-        if len(self.star_cache) == self.star_poly_order:
-            removed = self.star_cache.pop(0)
+        # Ensure coefficients do not exceed window_size, remove oldest bid and ask if it does
+        if len(self.star_cache_ask) == self.star_window_size:
+            self.star_cache_bid.pop(0)
+            self.star_cache_ask.pop(0)
 
         # Get the best buy and sell prices
         star_best_sell = min(state.order_depths['STARFRUIT'].sell_orders.keys())
         star_best_buy = max(state.order_depths['STARFRUIT'].buy_orders.keys())
 
-        # Update the cache with the new mid_price
-        self.star_cache.append((star_best_sell + star_best_buy) / 2)
+        # Update the caches with the new ask and bid prices
+        self.star_cache_bid.append(star_best_buy)
+        self.star_cache_ask.append(star_best_sell)
 
 
         star_band_lower = INF
         star_band_upper = INF
 
         # Use predicted next price to determine acceptable bids and asks
-        if len(self.star_cache) == self.star_poly_order:
-            star_band_lower = self.calc_next_price() - 1
-            star_band_upper = self.calc_next_price() + 1
+        if len(self.star_cache_ask) == self.star_window_size:
+            star_band_lower, star_band_upper = self.calc_next_price()
+            star_band_lower += 1
+            star_band_upper -= 1
             print(f"STARFRUIT BAND: {star_band_lower} - {star_band_upper}")
-
         else:
             print("Not enough data")
             return result, conversions, jsonpickle.encode(self)
@@ -136,17 +125,26 @@ class Trader:
         result['STARFRUIT'] = star_orders
         print(f"STARFRUIT: {star_orders}")
 
-        for product in state.own_trades.keys():
-            for trade in state.own_trades[product]:
-                if trade.timestamp != (state.timestamp-100):
-                    continue
+        if False:
+            for product in state.own_trades.keys():
+                for trade in state.own_trades[product]:
 
-            if trade.buyer == 'SUBMISSION':
-                self.cpnl[product] -= trade.price * trade.quantity
-            else:
-                self.cpnl[product] += trade.price * trade.quantity
-        
-        print(f"CUMULATIVE PNL: {self.cpnl['STARFRUIT']}")
+                    if trade.timestamp != (state.timestamp-100):
+                        continue
+
+                    if product == 'STARFRUIT':
+                        print(f"TRADE: buyer={trade.buyer}, seller={trade.seller}price={trade.price}, quantity={trade.quantity}, timestamp={trade.timestamp}")
+                        print(f"PREVIOUS PNL: {self.cpnl['STARFRUIT']}")
+                
+                    if trade.buyer == 'SUBMISSION':
+                        self.cpnl[product] -= (trade.price * trade.quantity)
+                        print(f"BUYING {trade.quantity} {product} at {trade.price} for {trade.price * trade.quantity}")
+                    else:
+                        self.cpnl[product] += (trade.price * trade.quantity)
+                        print(f"SELLING {trade.quantity} {product} at {trade.price} for {trade.price * trade.quantity}")
+                    print(f"CUMULATIVE PNL AFTER: {self.cpnl['STARFRUIT']}")
+            
+            print(f"CUMULATIVE PNL: {self.cpnl['STARFRUIT']}")
 
         return result, conversions, jsonpickle.encode(self)
 
