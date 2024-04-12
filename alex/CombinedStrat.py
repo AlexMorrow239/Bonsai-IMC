69974f97e545dbab83df2b8da586d619bbd95e9d
AlexMorrow239 <alex.morrow239@gmail.com>
2024-04-11 22:03:13 -0400

    Fix condition in CombinedStrat.py to check state.timestamp instead of length of am_historical_bid_prices

--- alex/CombinedStrat.py
+++ alex/CombinedStrat.py
@@ -11,12 +11,11 @@ import jsonpickle
 class Trader:
     
     def __init__(self):
-        self.position = copy.deepcopy({'STARFRUIT': 0, 'AMETHYSTS': 0})
+        self.position = {'STARFRUIT': 0, 'AMETHYSTS': 0}
         self.star_cache = []
         self.star_poly_order = 8
         self.SMOOTHING = 0.98
         self.am_window_size = 34
-        self.am_position_open = False
         self.am_position_type = None
         self.am_historical_bid_prices = []
         self.am_historical_ask_prices = []
@@ -85,10 +84,6 @@ class Trader:
         INF = int(1e9)
         conversions = 1
         result = {}
-        orders = {
-            'STARFRUIT': [],
-            'AMETHYSTS': []
-        }
 
         if state.traderData:
             saved_state = jsonpickle.decode(state.traderData)
@@ -99,7 +94,6 @@ class Trader:
             self.SMOOTHING = saved_state.SMOOTHING
             # AMETHYSTS
             self.am_window_size = saved_state.am_window_size
-            self.am_position_open = saved_state.am_position_open
             self.am_position_type = saved_state.am_position_type
             self.am_historical_bid_prices = saved_state.am_historical_bid_prices
             self.am_historical_ask_prices = saved_state.am_historical_ask_prices
@@ -141,61 +135,51 @@ class Trader:
 
         am_order_depth = state.order_depths['AMETHYSTS']
         am_orders = []
-
-        if am_order_depth.buy_orders and am_order_depth.sell_orders:
-            am_bid_price = max(am_order_depth.buy_orders)
-            self.am_historical_bid_prices.append(am_bid_price)
+        am_cur_position = self.position['AMETHYSTS']
 
         if state.timestamp > 2000:
 
             am_live_ask_price, am_live_ask_volume = list(am_order_depth.sell_orders.items())[0]
             am_live_bid_price, am_live_bid_volume = list(am_order_depth.buy_orders.items())[0]
 
-            if not self.am_position_open:
+            if am_cur_position == 0:
                 if am_live_bid_price > 10000:
                     self.am_open_order_volume = min(abs(am_live_bid_volume)+3, 20)
                     am_orders.append(Order('AMETHYSTS', am_live_bid_price-1, -self.am_open_order_volume))
                     self.am_latest_price = am_live_bid_price
-                    self.am_position_open = True
                     self.am_position_type = 'Short'
                     self.am_remaining_quantity = self.am_open_order_volume
                 elif am_live_ask_price < 10000:
                     self.am_open_order_volume = min(abs(am_live_ask_volume)+3, 20)
                     am_orders.append(Order('AMETHYSTS', am_live_ask_price+1, self.am_open_order_volume))
                     self.am_latest_price = am_live_ask_price
-                    self.am_position_open = True
                     self.am_position_type = 'Long'
                     self.am_remaining_quantity = self.am_open_order_volume
                     
-            elif self.am_position_open:
+            elif am_cur_position != 0:
                 if not self.am_partially_closed:
                     if self.am_position_type == 'Long' and am_live_ask_price < 10000:
                         am_additional_volume = min(abs(20-self.am_remaining_quantity), abs(am_live_ask_volume))
                         am_orders.append(Order('AMETHYSTS', am_live_ask_price, am_additional_volume))
                         self.am_remaining_quantity += am_additional_volume
-                        self.am_position_open = True
                         self.am_position_type = 'Long'
                     elif self.am_position_type == 'Short' and am_live_bid_price > 10000:
                         am_additional_volume = min(abs(20-self.am_remaining_quantity), abs(am_live_bid_volume))
                         am_orders.append(Order('AMETHYSTS', am_live_bid_price, -am_additional_volume))
                         self.am_remaining_quantity += am_additional_volume
-                        self.am_position_open = True
                         self.am_position_type = 'Short'
                     if self.am_position_type == 'Short' and (am_live_ask_price < 10000 or am_live_ask_price < self.am_latest_price):
                         am_closing_volume = min(abs(am_live_ask_volume)+3, 20+self.am_remaining_quantity)
                         am_orders.append(Order('AMETHYSTS', am_live_ask_price+1, am_closing_volume))
                         if self.am_remaining_quantity > am_closing_volume:
                             self.am_partially_closed = True
-                            self.am_position_open = True
                             self.am_position_type = 'Short'
                             self.am_remaining_quantity -= am_closing_volume
                         elif self.am_remaining_quantity == am_closing_volume:
-                            self.am_position_open = False
                             self.am_partially_closed = False
                             self.am_remaining_quantity -= am_closing_volume
                         elif self.am_remaining_quantity < am_closing_volume:
                             self.am_latest_price = am_live_ask_price
-                            self.am_position_open = True
                             self.am_partially_closed = False
                             self.am_position_type = 'Long'
                             self.am_remaining_quantity = (am_closing_volume - self.am_remaining_quantity)
@@ -205,16 +189,13 @@ class Trader:
                         am_orders.append(Order('AMETHYSTS', am_live_bid_price-1, -am_closing_volume))
                         if self.am_remaining_quantity > am_closing_volume:
                             self.am_partially_closed = True
-                            self.am_position_open = True
                             self.am_position_type = 'Long'
                             self.am_remaining_quantity -= am_closing_volume
                         elif self.am_remaining_quantity == am_closing_volume:
                             self.am_partially_closed = False
-                            self.am_position_open = False
                             self.am_remaining_quantity -= am_closing_volume
                         elif self.am_remaining_quantity < am_closing_volume:
                             self.am_latest_price = am_live_bid_price
-                            self.am_position_open = True
                             self.am_partially_closed = False
                             self.am_position_type = 'Short'
                             self.am_remaining_quantity = (am_closing_volume - self.am_remaining_quantity)
@@ -224,29 +205,24 @@ class Trader:
                         am_additional_volume = min(abs(20-self.am_remaining_quantity), abs(am_live_ask_volume))
                         am_orders.append(Order('AMETHYSTS', am_live_ask_price, am_additional_volume))
                         self.am_remaining_quantity += am_additional_volume
-                        self.am_position_open = True
                         self.am_position_type = 'Long'
                     elif self.am_position_type == 'Short' and am_live_bid_price > 10000:
                         am_additional_volume = min(abs(20-self.am_remaining_quantity), abs(am_live_bid_volume))
                         am_orders.append(Order('AMETHYSTS', am_live_bid_price, -am_additional_volume))
                         self.am_remaining_quantity += am_additional_volume
-                        self.am_position_open = True
                         self.am_position_type = 'Short'
                     if self.am_position_type == 'Short' and (am_live_ask_price < 10000 or am_live_ask_price < self.am_latest_price):
                         am_order_quantity = min(abs(am_live_ask_volume)+3, 20+self.am_remaining_quantity)
                         am_orders.append(Order('AMETHYSTS', am_live_ask_price+1, am_order_quantity))
                         if self.am_remaining_quantity > am_order_quantity:
                             self.am_partially_closed = True
-                            self.am_position_open = True
                             self.am_position_type = 'Short'
                             self.am_remaining_quantity -= am_order_quantity
                         elif self.am_remaining_quantity == am_order_quantity:
-                            self.am_position_open = False
                             self.am_partially_closed = False
                             self.am_remaining_quantity -= am_order_quantity
                         elif self.am_remaining_quantity < am_order_quantity:
                             self.am_latest_price = am_live_ask_price
-                            self.am_position_open = True
                             self.am_partially_closed = False
                             self.am_position_type = 'Long'
                             self.am_remaining_quantity = (am_order_quantity - self.am_remaining_quantity)
@@ -255,16 +231,13 @@ class Trader:
                         am_orders.append(Order('AMETHYSTS', am_live_bid_price-1, -am_order_quantity))
                         if self.am_remaining_quantity > am_order_quantity:
                             self.am_partially_closed = True
-                            self.am_position_open = True
                             self.am_position_type = 'Long'
                             self.am_remaining_quantity -= am_order_quantity
                         elif self.am_remaining_quantity == am_order_quantity:
                             self.am_partially_closed = False
-                            self.am_position_open = False
                             self.am_remaining_quantity -= am_order_quantity
                         elif self.am_remaining_quantity < am_order_quantity:
                             self.am_latest_price = am_live_bid_price
-                            self.am_position_open = True
                             self.am_partially_closed = False
                             self.am_position_type = 'Short'
                             self.am_remaining_quantity = (am_order_quantity - self.am_remaining_quantity)
