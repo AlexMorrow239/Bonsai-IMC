87246ceda7acbba4af0e388a39ac9e195e015144
AlexMorrow239 <alex.morrow239@gmail.com>
2024-04-14 02:48:10 -0400

    Minimized Run method

--- alex/CombinedStrat.py
+++ alex/CombinedStrat.py
@@ -11,11 +11,11 @@ import jsonpickle
 class Trader:
     
     def __init__(self):
+        # Global
         self.position = {'STARFRUIT': 0, 'AMETHYSTS': 0}
         # STARFRUIT
         self.star_cache = []
         self.star_window_size = 8
-        self.SMOOTHING = 0.99
         # AMETHYSTS
         self.am_remaining_quantity = 0
         self.am_partially_closed = False
@@ -28,53 +28,51 @@ class Trader:
 
         if state.traderData:
             saved_state = jsonpickle.decode(state.traderData)
-            # STARFRUIT
+            #Global
             self.position = saved_state.position
+            # STARFRUIT
             self.star_cache = saved_state.star_cache
             self.star_window_size = saved_state.star_window_size
-            self.SMOOTHING = saved_state.SMOOTHING
             # AMETHYSTS
             self.am_remaining_quantity = saved_state.am_remaining_quantity
             self.am_partially_closed = saved_state.am_partially_closed
             self.am_latest_price = saved_state.am_latest_price
 
+        # Update positions for each procuct
         for product in state.order_depths:
             self.position[product] = state.position.get(product, 0) # Update position
 
-        # Ensure coefficients do not exceed 12, remove oldest mid_price if it does
+        # STARFRUIT
+        # Ensure STARFRUIT coefficients do not exceed 12, remove oldest mid_price if it does
         if len(self.star_cache) == self.star_window_size:
             self.star_cache.pop(0)
-
         # Get the best buy and sell prices for STARFRUIT
         star_best_sell, star_best_buy = self.get_best_prices(state.order_depths['STARFRUIT'])
-        # Update the cache with the new mid_price
-        self.star_cache.append((star_best_sell + star_best_buy) / 2)
+        self.star_cache.append((star_best_sell + star_best_buy) / 2)    # Append the mid price to the cache
 
         star_band_lower = INF
         star_band_upper = INF
-
         star = True
-        # Use predicted next price to determine acceptable bids and asks
+        
         if len(self.star_cache) == self.star_window_size:
-            star_band_lower = self.calc_next_price() - 1.0
-            star_band_upper = self.calc_next_price() + 1.0
-
+            # Use predicted next price to determine acceptable bids and asks
+            star_band_lower = self.calc_next_price_regression() - 1.0
+            star_band_upper = self.calc_next_price_regression() + 1.0
+            star_orders = self.create_orders_regression('STARFRUIT', state.order_depths['STARFRUIT'], star_band_lower, star_band_upper, 19)
         else:
             print("Not enough data")
-            star = False
-
-        star_orders = self.create_orders_regression('STARFRUIT', state.order_depths['STARFRUIT'], star_band_lower, star_band_upper, 19) if star else []
+            star_orders = []
         result['STARFRUIT'] = star_orders
 
-
-        am_order_depth = state.order_depths['AMETHYSTS']
-
-        am_orders = []
-
+        # AMETHYSTS
         if state.timestamp > 2000:
+            am_order_depth = state.order_depths['AMETHYSTS']
+            am_orders = []
             am_orders = self.create_orders_amethysts(am_order_depth)
-            
+        else:
+            am_orders = []
         result['AMETHYSTS'] = am_orders
+
         return result, conversions, jsonpickle.encode(self)
     
     # Returns best_ask, best_bid
@@ -87,14 +85,14 @@ class Trader:
         else:
             return min(order_depth.sell_orders.keys()), max(order_depth.buy_orders.keys())
     
-    def filter(self, z: List) -> List:
+    def smooth_data(self, z: List, SMOOTHING) -> List:
             z_doubled = z
 
             z_fft = np.fft.fft(z_doubled)
             n = len(z_doubled)
             filter = []
             for i in range(n):
-                if i < (n / 2) *  (1 - self.SMOOTHING) or i > (n / 2) * (1 + self.SMOOTHING):
+                if i < (n / 2) *  (1 - SMOOTHING) or i > (n / 2) * (1 + SMOOTHING):
                     filter.append(1)
                 else:
                     filter.append(0)
@@ -103,9 +101,10 @@ class Trader:
             return z1
         
 
-    def calc_next_price(self):
+    def calc_next_price_regression(self):
 
-        smoothed_star_cache = self.filter(self.star_cache)
+        SMOOTHING = 0.99
+        smoothed_star_cache = self.smooth_data(self.star_cache, SMOOTHING)
         a,b,c,d,intercept = np.polyfit(range(0, len(smoothed_star_cache)), smoothed_star_cache, 4)
         
         t = len(self.star_cache) + self.star_window_size/4
@@ -157,7 +156,7 @@ class Trader:
             self.am_remaining_quantity = am_open_order_volume
         return orders
     
-    def handle_not_partially_closed(self, product, current_position, ask_price, bid_price, ask_volume, bid_volume):
+    def handle_not_partially_closed_high_frequency(self, product, current_position, ask_price, bid_price, ask_volume, bid_volume):
 
         orders = []
         if current_position > 0 and ask_price < 10000:
@@ -198,7 +197,7 @@ class Trader:
 
         return orders
     
-    def handle_partially_closed(self, product, current_position, ask_price, bid_price, ask_volume, bid_volume):
+    def handle_partially_closed_high_frequency(self, product, current_position, ask_price, bid_price, ask_volume, bid_volume):
         orders = []
         if current_position > 0 and ask_price < 10000:
             am_additional_volume = min(abs(20-self.am_remaining_quantity), abs(ask_volume)+3)
@@ -244,10 +243,10 @@ class Trader:
         if am_cur_position == 0:
             am_orders = self.open_order_high_frequency(am_live_bid_price, am_live_ask_price, am_live_bid_volume, am_live_ask_volume)
         elif am_cur_position != 0 and not self.am_partially_closed:
-            am_orders = self.handle_not_partially_closed('AMETHYSTS', am_cur_position, am_live_ask_price, am_live_bid_price, am_live_ask_volume, am_live_bid_volume)
+            am_orders = self.handle_not_partially_closed_high_frequency('AMETHYSTS', am_cur_position, am_live_ask_price, am_live_bid_price, am_live_ask_volume, am_live_bid_volume)
                         
         elif am_cur_position != 0 and self.am_partially_closed:
-            am_orders = self.handle_partially_closed('AMETHYSTS', am_cur_position, am_live_ask_price, am_live_bid_price, am_live_ask_volume, am_live_bid_volume)
+            am_orders = self.handle_partially_closed_high_frequency('AMETHYSTS', am_cur_position, am_live_ask_price, am_live_bid_price, am_live_ask_volume, am_live_bid_volume)
         return am_orders
 
     
